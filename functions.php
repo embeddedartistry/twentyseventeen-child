f35dfca8716f44ed216c46f19ca1106975313595
Phillip Johnston <phillip@embeddedartistry.com>
2021-10-18 11:58:47 -0700

    Check if this fixes it

--- functions.php
+++ functions.php
@@ -1,4 +1,18 @@
 <?php
+
+function child_theme_enqueue_styles() {
+
+    $parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+}
+add_action( 'wp_enqueue_scripts', 'child_theme_enqueue_styles' );
+
 /**
  * Prints HTML with meta information for the categories, tags and comments.
  */
