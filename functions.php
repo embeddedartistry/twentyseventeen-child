5a8ebfbea4f09776c6a7e3f18bdd5776131d1bfa
Phillip Johnston <phillip@embeddedartistry.com>
2023-05-09 19:47:27 -0700

    Cleanup around style sheet enqueuing for child theme

--- functions.php
+++ functions.php
@@ -2,13 +2,12 @@
 
 function child_theme_enqueue_styles() {
 
-    $parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+    $parent_style = 'twentyseventeen-style';
 
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style ),
-        wp_get_theme()->get('Version')
+    wp_enqueue_style( 'twentyseventeen-child-style',
+        get_stylesheet_uri(),
+        array( $parent_style )
     );
 }
 add_action( 'wp_enqueue_scripts', 'child_theme_enqueue_styles' );
